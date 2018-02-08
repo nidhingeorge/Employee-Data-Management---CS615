0b61a45e099b7ae157de20334a1dd72fbdca8702
nidhingeorge <36041905+nidhingeorge@users.noreply.github.com>
2018-02-08 16:12:11 +0000

    Removed the additional code for MySQL

--- config.php
+++ config.php
@@ -20,33 +20,5 @@
 $connectionInfo = array("UID" => "user@crudedbserver", "pwd" => "pwd12%%12", "Database" => "CrudeDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:crudedbserver.database.windows.net,1433";
 $pdo = sqlsrv_connect($serverName, $connectionInfo);
-
-
-
-/* Attempt to connect to MySQL database */
-/*
-$connectstr_dbhost = 'tcp:crudedbserver.database.windows.net,1433';
-$connectstr_dbname = 'CrudeDB';
-$connectstr_dbusername = 'user';
-$connectstr_dbpassword = 'pwd12%%12';
-// MS Azure does not allow direct access to MySQL configuration, only via environment
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-$pdo = sqlsrv_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-if (!$pdo) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}*/
-// TODO make sure DB exists
-// TODO create table (only once, if it does not yet exist)
 ?>
+
