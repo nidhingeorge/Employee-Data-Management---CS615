b2a38a22e274ff8c1d3536027ad5414807ffaa24
nidhingeorge <36041905+nidhingeorge@users.noreply.github.com>
2018-02-08 17:06:49 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,27 @@
+/*
 <?php
 
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'user');
+define('DB_NAME', 'user');
+ 
+/* Attempt to connect to MySQL database */
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+?>
+
+
+<?php
+*/
 
 // PHP Data Objects(PDO) Sample Code:
 
